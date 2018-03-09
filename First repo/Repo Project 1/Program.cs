60c97bfbb63bb9ff325697e10c2db69773719fbb
Milena Aleksieva <milenaaleksieva@abv.bg>
2018-03-09 15:06:55 +0200

    Create of .cs file with message

---
+++ First repo/Repo Project 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Repo_Project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My first repository");
+            Console.WriteLine("The programing is cool :)");
+        }
+    }
+}
